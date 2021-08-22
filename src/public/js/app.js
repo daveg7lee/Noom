6ed8748c5c235e813b988b67e6736c3086cb9a4f
Dave <daveg7lee@gmail.com>
2021-08-22 11:41:02 +0900

    Room Notifications

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,11 +15,22 @@ const addMsg = (msg) => {
   ul.appendChild(li);
 };
 
+const handleMsgSubmit = (e) => {
+  e.preventDefault();
+  const input = room.querySelector('input');
+  socket.emit('new_message', input.value, roomName, () => {
+    addMsg(`You: ${input.value}`);
+    input.value = '';
+  });
+};
+
 const showRoom = () => {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector('h3');
   h3.innerHTML = `Room ${roomName}`;
+  const form = room.querySelector('form');
+  form.addEventListener('submit', handleMsgSubmit);
 };
 
 const handleRoomSubmit = (e) => {
@@ -35,3 +46,9 @@ form.addEventListener('submit', handleRoomSubmit);
 socket.on('welcome', () => {
   addMsg('Someone joined');
 });
+
+socket.on('bye', () => {
+  addMsg('Someone left');
+});
+
+socket.on('new_message', addMsg);
--- src/server.js
+++ src/server.js
@@ -21,6 +21,13 @@ io.on('connection', (socket) => {
     done();
     socket.to(roomName).emit('welcome');
   });
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => socket.to(room).emit('bye'));
+  });
+  socket.on('new_message', (msg, roomName, done) => {
+    socket.to(roomName).emit('new_message', msg);
+    done();
+  });
 });
 
 server.listen(3000, handleListen);
