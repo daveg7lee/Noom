17fe8489a5dde6c0a89ea5765a52c4a19d9c9763
Dave <daveg7lee@gmail.com>
2021-08-22 20:00:10 +0900

    User Video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,74 +1,47 @@
 const socket = io();
 
-const welcome = document.getElementById('welcome');
-const form = welcome.querySelector('form');
-const room = document.getElementById('room');
+const myFace = document.getElementById('myFace');
+const muteButton = document.getElementById('mute');
+const cameraButton = document.getElementById('camera');
 
-room.hidden = true;
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-let roomName;
-
-function updateTitle(roomName, newCount) {
-  const h3 = room.querySelector('h3');
-  h3.innerHTML = `Room ${roomName} (${newCount})`;
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+    console.log(myStream);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-const addMsg = (msg) => {
-  const ul = room.querySelector('ul');
-  const li = document.createElement('li');
-  li.innerText = msg;
-  ul.appendChild(li);
-};
+getMedia();
 
-const handleMsgSubmit = (e) => {
-  e.preventDefault();
-  const input = room.querySelector('input');
-  socket.emit('new_message', input.value, roomName, () => {
-    addMsg(`You: ${input.value}`);
-    input.value = '';
-  });
-};
+function handleMuteClick() {
+  if (!muted) {
+    muteButton.innerText = 'Unmute';
+    muted = true;
+  } else {
+    muteButton.innerText = 'Mute';
+    muted = false;
+  }
+}
 
-const showRoom = (newCount) => {
-  updateTitle(roomName, newCount);
-  const msgForm = room.querySelector('form');
-  welcome.hidden = true;
-  room.hidden = false;
-  msgForm.addEventListener('submit', handleMsgSubmit);
-};
+function handleCameraClick() {
+  if (cameraOff) {
+    cameraButton.innerText = 'Turn Camera Off';
+    cameraOff = false;
+  } else {
+    cameraButton.innerText = 'Turn Camera On';
+    cameraOff = true;
+  }
+}
 
-const handleWelcomeSubmit = (e) => {
-  e.preventDefault();
-  const roomNameInput = form.querySelector('#roomName');
-  const nicknameInput = form.querySelector('#nickname');
-  socket.emit('nickname', nicknameInput.value);
-  socket.emit('enter_room', roomNameInput.value, showRoom);
-  roomName = roomNameInput.value;
-  nickname = nicknameInput.value;
-  roomNameInput.value = '';
-  nicknameInput.value = '';
-};
-
-form.addEventListener('submit', handleWelcomeSubmit);
-
-socket.on('welcome', (nickname, newCount) => {
-  updateTitle(roomName, newCount);
-  addMsg(`${nickname} joined`);
-});
-
-socket.on('bye', (nickname, newCount) => {
-  updateTitle(roomName, newCount);
-  addMsg(`${nickname} left`);
-});
-
-socket.on('new_message', addMsg);
-
-socket.on('room_change', (rooms) => {
-  const roomList = welcome.querySelector('ul');
-  roomList.innerHTML = '';
-  rooms.forEach((room) => {
-    const li = document.createElement('li');
-    li.innerText = room;
-    roomList.appendChild(li);
-  });
-});
+muteButton.addEventListener('click', handleMuteClick);
+cameraButton.addEventListener('click', handleCameraClick);
--- src/server.js
+++ src/server.js
@@ -25,48 +25,4 @@ instrument(io, {
   auth: false,
 });
 
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return io.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-io.on('connection', (socket) => {
-  socket['nickname'] = 'Anon';
-  socket.on('enter_room', (roomName, done) => {
-    socket.join(roomName);
-    done(countRoom(roomName));
-    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
-    io.sockets.emit('room_change', publicRooms());
-  });
-  socket.on('disconnecting', () => {
-    socket.rooms.forEach(
-      (room) =>
-        socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1),
-      socket.nickname
-    );
-  });
-  socket.on('disconnect', () => {
-    io.sockets.emit('room_change', publicRooms());
-  });
-  socket.on('new_message', (msg, roomName, done) => {
-    socket.to(roomName).emit('new_message', `${socket.nickname}: ${msg}`);
-    done();
-  });
-  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
-});
-
 server.listen(3000, handleListen);
