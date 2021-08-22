57ba9c9f26b5c1c5f71f40e118f46d4c5730d91b
Dave <daveg7lee@gmail.com>
2021-08-22 19:45:59 +0900

    Admin Panel

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import SocketIO from 'socket.io';
+import { Server } from 'socket.io';
 import http from 'http';
+import { instrument } from '@socket.io/admin-ui';
 
 const app = express();
 
@@ -13,7 +14,16 @@ app.get('/*', (req, res) => res.redirect('/'));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-const io = SocketIO(server);
+const io = new Server(server, {
+  cors: {
+    origin: ['https://admin.socket.io'],
+    credentials: true,
+  },
+});
+
+instrument(io, {
+  auth: false,
+});
 
 function publicRooms() {
   const {
